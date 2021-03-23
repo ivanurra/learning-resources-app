ff3d0efa4fd89b9e3d2eeb9a7fa1122eb8f771a0
ivanurra <ivan.urra.dev@gmail.com>
2021-03-23 16:45:26 +0100

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-    <ul>
-        <learning-resource 
-        v-for="res in storedResources" 
-        :key="res.id" 
-        :title="res.title" 
-        :description="res.description" 
-        :link="res.link"
-        ></learning-resource>
-    </ul>
+    <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 export default {
     components: {
-        LearningResource
+        StoredResources,
     },
     data() {
         return {
@@ -30,9 +22,25 @@ export default {
                 title: 'Google',
                 description: 'Learn to Google...',
                 link: 'https://google.com'
-                },
+                }
             ]
         };
     }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
