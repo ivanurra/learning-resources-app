31f1eccbaedb95af986b788b07ee83dc9abb8af6
ivanurra <ivan.urra.dev@gmail.com>
2021-03-24 12:14:55 +0100

    added styling tabs

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,16 +1,54 @@
 <template>
     <base-card>
-        <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
-        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+        <base-button @click="setSelectedTab('stored-resources')" 
+        :mode="storedResButtonMode"
+        >Stored Resources</base-button>
+        <base-button @click="setSelectedTab('add-resources')"
+        :mode="AddResButtonMode"
+        >Add Resource</base-button>
     </base-card>
+    <component :is="selectedTab"></component>
 </template>
 
 <script>
+import StoredResources from './StoredResources.vue'
+import AddResources from './AddResource.vue'
 export default {
+    components: {
+        StoredResources,
+        AddResources
+    },
     data() {
         return {
-            selectedTab: 'stored-resources'
+            selectedTab: 'stored-resources',
+            storedResources: [
+                { 
+                id: 'official-guide', 
+                title: 'Official Guide',
+                description: 'The official Vue.js documentation.',
+                link: 'https://vuejs.org'
+                },
+                { 
+                id: 'google', 
+                title: 'Google',
+                description: 'Learn to Google...',
+                link: 'https://google.com'
+                }
+            ]
         };
+    },
+    provide() {
+        return {
+            resources: this.storedResources
+        };
+    },
+    computed:{
+        storeResButtonMode() {
+            return this.selectedTab === 'stored-resources' ? null : 'flat';
+        },
+        addResButtonMode() {
+            return this.selectedTab === 'add-resource' ? null : 'flat'
+        }
     },
     methods: {
         setSelectedTab(tab) {
